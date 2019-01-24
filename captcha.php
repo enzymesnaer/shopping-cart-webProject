e348c5ccf112decd438a15396bf931c084f91aa0
enzymesnaer <snehar16395@gmail.com>
2019-01-24 19:53:22 +0530

    registrationCaptcha

---
+++ captcha.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+$possible = '0123456789bcdfghjkmnpqrstvwxyz';
+$text = '';
+$i = 0;
+while ($i < 5) {
+  $text .= " ".substr($possible, mt_rand(0,strlen($possible)-1), 1);
+  $i++;
+}
+$_SESSION["vercode"] = $text;
+$height = 30;
+$width = 150;
+$image_p = imagecreate($width, $height);
+$white = imagecolorallocate($image_p, 0, 0, 0);
+$white = imagecolorallocate($image_p,255,255,255);
+$font_size = 25;
+imagestring($image_p, 20, 18, 5, $text, $white);
+imagejpeg($image_p,null,80);
+?>
